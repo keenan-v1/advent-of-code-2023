eee8097968631d1e2f9662cbcbaa3c4cceb093dd
Keenan <jonathan@wantsmore.coffee>
2023-12-10 11:52:25 -0500

    Day 1 complete

--- src/day1/src/lib.rs
+++ src/day1/src/lib.rs
@@ -2,22 +2,78 @@ extern crate regex;
 
 use regex::Regex;
 
-pub fn day1(input: String) {
-    let result = day1_impl(input);
-    println!("The result is {}", result);
+pub fn day1() {
+    let input = std::fs::read_to_string("inputs/day1.txt").unwrap();
+    let lines = input.trim_matches('\n').split('\n').collect::<Vec<&str>>();
+    let result = day1_part1(&lines);
+    println!("Part 1: The result is {}", result);
+    let result = day1_part2(&lines);
+    println!("Part 2: The result is {}", result);
 }
 
-// We need to write a function for day 1, which takes a string as input and prints the result.
-fn day1_impl(input: String) -> usize {
-    let lines = input.split('\n');
+fn day1_part1(lines: &Vec<&str>) -> usize {
     let mut result = 0;
     for line in lines {
-        // use regex to find all numbers in the string
-        let numbers = Regex::new(r"\d+").unwrap();
-        // get the first and last number of the string and add them together as the result
-        let mut numbers = numbers.find_iter(line)
-            .map(|x| x.as_str().parse::<usize>().unwrap());
-        result += numbers.next().unwrap_or(0) + numbers.last().unwrap_or(0);
+        let numbers = Regex::new(r"\d").unwrap();
+        let mut numbers = numbers
+            .find_iter(line)
+            .map(|x| x.as_str().parse::<usize>());
+        let first = numbers.next().unwrap_or(Ok(0)).unwrap_or(0);
+        let last = numbers.last().unwrap_or(Ok(first)).unwrap_or(0);
+        assert!(0 < first && first < 10);
+        assert!(0 < last && last < 10);
+        result += first * 10 + last;
+    }
+    return result;
+}
+
+fn day1_part2(lines: &Vec<&str>) -> usize {
+    let mut result = 0;
+    let word_to_num = [
+        ("one", "1"),
+        ("two", "2"),
+        ("three", "3"),
+        ("four", "4"),
+        ("five", "5"),
+        ("six", "6"),
+        ("seven", "7"),
+        ("eight", "8"),
+        ("nine", "9"),
+    ];
+
+    for line in lines {
+        let mut new_line = String::new();
+        let mut index = 0;
+        while index < line.len() {
+            if let Some(ch) = line.chars().nth(index) {
+                if ch.is_digit(10) {
+                    new_line.push(ch);
+                    index += 1;
+                } else {
+                    let mut found = false;
+                    for (word, num) in word_to_num.iter() {
+                        if line[index..].starts_with(word) {
+                            new_line.push_str(num);
+                            index += 1;
+                            found = true;
+                            break;
+                        }
+                    }
+                    if !found {
+                        index += 1;
+                    }
+                }
+            }
+        }
+        let numbers = Regex::new(r"\d").unwrap();
+        let mut numbers = numbers
+            .find_iter(new_line.as_str())
+            .map(|x| x.as_str().parse::<usize>());
+        let first = numbers.next().unwrap().unwrap();
+        let last = numbers.last().unwrap_or(Ok(first)).unwrap();
+        assert!(0 < first && first < 10);
+        assert!(0 < last && last < 10);
+        result += first * 10 + last;
     }
     return result;
 }
@@ -27,9 +83,19 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let input = String::from("a1b2c3d\n4e5f6g7h8i9j\n");
-        let result = day1(input);
-        assert_eq!(result, 17);
+    fn part1() {
+        let input = String::from("\n1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet\n");
+        let lines = input.trim_matches('\n').split('\n').collect::<Vec<&str>>();
+        let result = day1_part1(&lines);
+        assert_eq!(result, 142);
+    }
+
+    #[test]
+    fn part2() {
+        let input = String::from("two1nine\neightwothree\nabcone2threexyz\nxtwone3four\
+        \n4nineeightseven2\nzoneight234\n7pqrstsixteen\noneight2nineight");
+        let lines = input.trim_matches('\n').split('\n').collect::<Vec<&str>>();
+        let result = day1_part2(&lines);
+        assert_eq!(result, 281 + 18);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,10 @@ extern crate day1;
 #[tokio::main]
 async fn main() {
     let args: Vec<String> = env::args().collect();
-    let input_url = &args[1];
-    let day = input_url.split("/").nth(5).unwrap();
-    let response = reqwest::get(input_url).await.unwrap();
-    let input = response.text().await.unwrap();
+    let day = &args[1].parse::<usize>().unwrap();
+    println!("Running day {}", day);
     match day {
-        "1" => day1::day1(input),
+        1 => day1::day1(),
         _ => println!("Invalid day"),
     }
-
-    println!("Hello, world!");
 }
